43aefbbe61bbd2a30b2b67665f44c76fd8d099a9
Ding Yu Lee <dingyu@logflows.com>
2022-11-25 12:18:33 +0800

    UPDAGE:hookize the step func

---
+++ src/app/hooks/useFabricCanvs.tsx
@@ -0,0 +1,22 @@
+import customFabricDeleteIcon from '@base/utils/helper'
+import { fabric } from 'fabric'
+import { useEffect, useRef, useState } from 'react'
+
+const useFabricCanvas = () => {
+    // init render state
+    const initRender = useRef(true)
+    // variables between re-render
+    const [fabricCanvas, setFabricCanvas] = useState<fabric.Canvas | null>(null)
+    useEffect(() => {
+        if (initRender.current) {
+            setFabricCanvas(new fabric.Canvas('file-canvas'))
+            customFabricDeleteIcon()
+        }
+        return () => {
+            initRender.current = false
+        }
+    }, [])
+    return fabricCanvas
+}
+
+export default useFabricCanvas
--- src/app/pages/SignPage.tsx
+++ src/app/pages/SignPage.tsx
@@ -1,98 +1,19 @@
 import SignModal from '@base/features/signData/components/SignModal'
-import customFabricDeleteIcon from '@base/utils/helper'
-import { FuncBarOptions, Step } from '@base/utils/types'
+import { Step } from '@base/utils/types'
+import BasicLayout from '@components/layouts/BasicLayout'
+import FunctionBar from '@components/layouts/FunctionBar'
+import ProgressLine from '@components/ProgressLine'
 import PreviewArea from '@features/pdfFile/PreviewArea'
-import { fabric } from 'fabric'
-import React, { useEffect, useRef, useState } from 'react'
-import { useNavigate } from 'react-router'
+import React, { useState } from 'react'
 
-import BasicLayout from '../components/layouts/BasicLayout'
-import FunctionBar from '../components/layouts/FunctionBar'
-import ProgressLine from '../components/ProgressLine'
 import ConvasContext from '../contexts/CanvasContext'
-import { useAppSelector } from '../hooks'
+import useFabricCanvas from '../hooks/useFabricCanvs'
+import useStepFunc from '../hooks/useStepFunc'
 
 export default function SignPage() {
-    const step1FuncBar = {
-        pen: () => {
-            alert(`請先確認是否使用${fileName}文件進行操作`)
-        },
-        text: () => {
-            alert(`請先確認是否使用${fileName}文件進行操作`)
-        },
-        date: () => {
-            alert(`請先確認是否使用${fileName}文件進行操作`)
-        },
-        checked: () => {
-            alert(`請先確認是否使用${fileName}文件進行操作`)
-        },
-        btn1: {
-            text: '取消',
-            func: () => {
-                navigate(-1)
-            },
-        },
-        btn2: {
-            text: '確認',
-            func: () => {
-                setStep(Step.two)
-            },
-        },
-    }
-    const step2FuncBar = {
-        pen: () => {
-            setShowModal(true)
-        },
-        text: () => {
-            alert(`請先確認是否使用${fileName}文件進行操作`)
-        },
-        date: () => {
-            alert(`請先確認是否使用${fileName}文件進行操作`)
-        },
-        checked: () => {
-            alert(`請先確認是否使用${fileName}文件進行操作`)
-        },
-        btn1: {
-            text: '取消',
-            func: () => {
-                navigate(-1)
-            },
-        },
-        btn2: {
-            text: '下一步',
-            func: () => {
-                setStep(Step.two)
-            },
-        },
-    }
-    const navigate = useNavigate()
-    const [step, setStep] = useState<Step>(Step.one)
-    const [stepFuncBar, setStepFuncBar] = useState<FuncBarOptions>(step1FuncBar)
-    const fileName = useAppSelector((state) => state.pdf.value.name)
-
+    const fabricCanvas = useFabricCanvas()
     const [showModal, setShowModal] = useState(false)
-
-    useEffect(() => {
-        if (step === Step.two) {
-            setStepFuncBar(step2FuncBar)
-        } else {
-            setStepFuncBar(step1FuncBar)
-        }
-    }, [step])
-
-    // init render state
-    const initRender = useRef(true)
-    // variables between re-render
-    const [fabricCanvas, setFabricCanvas] = useState<fabric.Canvas | null>(null)
-    useEffect(() => {
-        if (initRender.current) {
-            setFabricCanvas(new fabric.Canvas('file-canvas'))
-            customFabricDeleteIcon()
-        }
-        return () => {
-            initRender.current = false
-        }
-    }, [])
+    const { step, stepFuncBar } = useStepFunc(setShowModal)
 
     return (
         <BasicLayout>
